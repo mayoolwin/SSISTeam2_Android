05cb5aac33762ec21d2a2c0396d467da3347101b
HengTiong <heng_tiong@hotmail.com>
2017-07-26 10:45:30 +0800

    working on monthlyCheck activities

--- app/src/main/java/com/example/mayoolwin/ssisteam2_android/MainActivity.java
+++ app/src/main/java/com/example/mayoolwin/ssisteam2_android/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.mayoolwin.ssisteam2_android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Testing git again
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        String test = "clerk";
+        if (test == "clerk")
+            getMenuInflater().inflate(R.menu.clerk_menu, menu);
+        else if (test == "employee")
+            getMenuInflater().inflate(R.menu.employee_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.clerk_monthly:
+                startActivity(new Intent(this, MonthlyCheckActivity.class));
+                return true;
+        }
+        return true;
     }
 }
