4608698fd98ff3054a8b61a6bcd4878eeee64373
waddy <shwewaddylwin@gmail.com>
2017-07-26 11:06:41 +0800

    Commit Changes by Waddy

--- app/src/main/java/com/example/mayoolwin/ssisteam2_android/MainActivity.java
+++ app/src/main/java/com/example/mayoolwin/ssisteam2_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mayoolwin.ssisteam2_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Testing git again
+
+
     }
 }
---
+++ app/src/main/java/com/example/mayoolwin/ssisteam2_android/Request.java
@@ -0,0 +1,70 @@
+package com.example.mayoolwin.ssisteam2_android;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by macbook on 25/7/17.
+ */
+
+public class Request extends HashMap<String,String> {
+
+    final static String host = "http://172.23.135.134/SSISAndroidTeam2/Service.svc";
+
+
+    public Request(String id, String name, String date) {
+        put("Id", id);
+        put("Name", name);
+        put("Date", date_time);
+
+    }
+
+    public Request(String id, String name, String date, String status, String item_description, String quantity){}
+
+    public static List<String> listRequest() {
+        List<String> list = new ArrayList<String>();
+        try {
+            JSONArray a = JSONParser.getJSONArrayFromUrl(host+"/Request");
+            for (int i=0; i<a.length(); i++) {
+                String c = a.getString(i);
+                list.add(c);
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public static Request getRequest(String id) {
+        Request request = null;
+        try {
+            JSONObject c = JSONParser.getJSONFromUrl(host+"/Request/"+id);
+            request = new Request(c.getString("req_id"),
+                    c.getString("requestdate"),
+                    c.getString("user"));
+
+        } catch (Exception e) {
+        }
+        return request;
+    }
+
+   /* public static void updateRequest(Request r) {
+        JSONObject jrequest = new JSONObject();
+        try {
+            jrequest.put("Id", r.get("Id"));
+            jrequest.put("Name", r.get("Name"));
+            jrequest.put("Date", r.get("Date"));
+            jrequest.put("Status", r.get("Status"));
+            jrequest.put("Item Description", r.get("Item Description"));
+            jrequest.put("Quantity", Integer.parseInt(r.get("Quantity")));
+        } catch (Exception e) {
+        }
+        String result = JSONParser.postStream(host+"/Update", jrequest.toString());
+    }*/
+
+
+}
