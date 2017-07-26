52cbd357aa9800ea1def4ef137e1c3fe43c49660
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-07-26 14:58:04 +0800

    Request Changes

--- app/src/main/java/com/example/mayoolwin/ssisteam2_android/ViewAllPendingRequestActivity.java
+++ app/src/main/java/com/example/mayoolwin/ssisteam2_android/ViewAllPendingRequestActivity.java
@@ -2,35 +2,46 @@ package com.example.mayoolwin.ssisteam2_android;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.List;
 
 public class ViewAllPendingRequestActivity extends ListActivity {
 
+    SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new AsyncTask<Void,Void,List<String>>()
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        String dept_code= pref.getString("dept_code", "default");
+
+        new AsyncTask<String,Void,List<Request>>()
         {
 
             @Override
-            protected List<String> doInBackground(Void... params) {
-                List<String> r=Request.listRequest();
+            protected List<Request> doInBackground(String... params) {
+                List<Request> r=Request.listRequest(params[0]);
                 return r;
             }
 
             @Override
-            protected void onPostExecute(List<String> result) {
+            protected void onPostExecute(List<Request> result) {
+
+               setListAdapter(new SimpleAdapter(getApplicationContext(), result,android.R.layout.simple_list_item_2,new String[]{"Name","Date"},new int[]{android.R.id.text1,android.R.id.text2}));
+
 
             }
-        }.execute();
+        }.execute(dept_code);
     }
     protected void onListItemClick(ListView l, View v, int postion, long id)
     {
