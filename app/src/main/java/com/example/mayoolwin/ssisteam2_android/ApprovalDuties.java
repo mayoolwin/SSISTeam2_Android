bfc7b255765798f0b8891309a483a9c7c69c6df4
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-07-28 21:18:28 +0800

    Change Ip Address

--- app/src/main/java/com/example/mayoolwin/ssisteam2_android/ApprovalDuties.java
+++ app/src/main/java/com/example/mayoolwin/ssisteam2_android/ApprovalDuties.java
@@ -11,17 +11,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static com.example.mayoolwin.ssisteam2_android.User.host;
+
 /**
  * Created by May Oo Lwin on 7/26/2017.
  */
 
 public class ApprovalDuties extends java.util.HashMap<String,String> {
-    //final static String host = "http:// 192.168.1.14:8090/DelegateAuthorityWebService/Service.svc";
-    //final static String host = "http://172.23.135.69/DelegateAuthorityWebService/Service.svc";
 
-    //final static String host = "http://172.23.135.222/SSISTeam2/Classes/WebServices/Service.svc";
-//    final static String host = "http://172.23.135.128/SSISTeam2/Classes/WebServices/Service.svc";
-    final static String host = "http://172.23.134.205/SSISTeam2/Classes/WebServices/Service.svc";
 
     public ApprovalDuties(String username, String startDate, String endDate, String deptCode,String createdDate,String deleted,String reason) {
         put("UserName", username);
